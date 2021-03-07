cb77bcc53989810b7ddbf2919ba2b39745008459
Jola-1 <adunolamjay6@gmail.com>
2021-03-07 23:46:02 +0100

    to get vanila api working

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import polls_list, polls_detail
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('polls/', polls_list, name = 'polls_list'),
+    path('polls/<int:pk>', polls_detail, name = 'polls_detail')
 ]

ce2f11140ceaa65c00d7eae9b2cfe0cdb7a1cde3
Jola-1 <adunolamjay6@gmail.com>
2021-03-08 17:28:37 +0100

    correcting a bug

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', PollList.as_view(), name = 'polls_list'),
     path('polls/<int:pk>', PollDetail.as_view(), name = 'polls_detail'),
-    path('choices/' ChoiceList.as_view(), name = "choice_list"),
+    path('choices/', ChoiceList.as_view(), name = "choice_list"),
     path('vote/', CreateVote.as_view(), name= 'Create_vote')
 ]

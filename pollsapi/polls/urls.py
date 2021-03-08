53c3effed4f4229e75d9e42c07dce68c4742b723
Jola-1 <adunolamjay6@gmail.com>
2021-03-08 17:26:30 +0100

    adding more classes to the view

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .apiviews import PollList, PollDetail 
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', PollList.as_view(), name = 'polls_list'),
-    path('polls/<int:pk>', PollDetail.as_view(), name = 'polls_detail')
+    path('polls/<int:pk>', PollDetail.as_view(), name = 'polls_detail'),
+    path('choices/' ChoiceList.as_view(), name = "choice_list"),
+    path('vote/', CreateVote.as_view(), name= 'Create_vote')
 ]

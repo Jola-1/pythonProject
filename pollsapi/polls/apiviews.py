c8f5dc5b8d07054c2eea446c75b1155887a5fcc9
Jola-1 <adunolamjay6@gmail.com>
2021-03-08 16:26:41 +0100

    correcting mistake 2

--- pollsapi/polls/apiviews.py
+++ pollsapi/polls/apiviews.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 
-from .models import Poll, Choices
+from .models import Poll, Choice
 from .serializers import PollSerializer
 
 class PollList(APIView):

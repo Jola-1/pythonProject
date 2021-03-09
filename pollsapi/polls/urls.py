0e2aaea59b214cc11056a279b9a4e332ed63a8a6
Jola-1 <adunolamjay6@gmail.com>
2021-03-09 14:04:03 +0100

    restructuring url

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', PollList.as_view(), name = 'polls_list'),
     path('polls/<int:pk>', PollDetail.as_view(), name = 'polls_detail'),
-    path('choices/', ChoiceList.as_view(), name = "choice_list"),
-    path('vote/', CreateVote.as_view(), name= 'Create_vote')
+    path('polls/<int:pk>/choices/', ChoiceList.as_view(), name = "choice_list"),
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name= 'Create_vote')
 ]
